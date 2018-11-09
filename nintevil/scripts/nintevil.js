265dc2aef0d0f14c348fef9592fec1a40ed77b34
Sushygore <35738055+Sushygore@users.noreply.github.com>
2018-11-09 09:36:26 -0500

    Name change

--- nintevil/scripts/nintevil.js
+++
@@ -1,5 +0,0 @@
-var myLine = document.querySelectorAll('li > :first-child,.dropdown-item');
-var icone = '<i class="fa fa-gamepad"></i>';
-myLine.forEach(element => {
-    element.innerHTML = icone + element.innerHTML;
-});
